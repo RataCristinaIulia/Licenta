3f2de404a31c22cee70bc9ed3b99a3db71d019e1
Rata Cristina Iulia <cristinaiulia.rata@gmail.com>
2022-06-05 19:38:12 +0300

    add func: generate population, mutation, cross, prototype ga

--- main.py
+++ main.py
@@ -10,6 +10,15 @@ test_x = []
 test_y = []
 k = 3
 
+population = []
+new_population = []
+no_individuals = 100
+mutation_chance = 0.01
+cross_chance = 0.3
+
+eval = []
+current_index = 0
+
 
 def read_keel_file(file_path):
     with open(file_path, 'r') as file:
@@ -80,12 +89,20 @@ def generate_procentages(train_set):
     return procentages
 
 
+def generate_population(train_set):
+    for i in range(0, no_individuals):
+        eval.append(1)
+        procentages = generate_procentages(train_set)
+        population.append(procentages)
+
+
 def my_weights(weights):
-    global train_x, test_x, k
+    global train_x, test_x, k, current_index
     # print("Weights: \n", weights)
 
     indexes_nearest_neighbors = get_indexes_nearest_neighbors(train_x, test_x, k)
-    procentages = generate_procentages(train_x)
+    procentages = population[current_index]
+    current_index += 1
 
     for i in range(0, len(weights)):
         for j in range(0, len(weights[i])):
@@ -106,19 +123,86 @@ def get_indexes_nearest_neighbors(train_x, test_x, k):
     return indexes_nearest_neighbors
 
 
+def mutation(individ):
+    prob = random.uniform(0, 1)
+    if prob < mutation_chance:
+        rand_pos = random.randint(0, len(individ))
+        new_procentage = random.uniform(0, 1)
+        individ[rand_pos] = new_procentage
+    return individ
+
+
+def crossing(crom1, crom2):
+    prob = random.uniform(0, 1)
+    if prob < cross_chance:
+        crossing_poz = random.randint(0, len(crom1))
+        child1 = crom1[0:crossing_poz] + crom2[crossing_poz:len(crom2)]
+        child2 = crom2[0:crossing_poz] + crom1[crossing_poz:len(crom1)]
+
+
 def genetic_algorithm():
-    global train_x, train_y, test_x, test_y, k
-    neight = KNeighborsClassifier(n_neighbors=k, weights=my_weights)
-    neight.fit(train_x, train_y)
+    global train_x, train_y, test_x, test_y, k, current_index
+    # neight = KNeighborsClassifier(n_neighbors=k, weights=my_weights)
+    # neight.fit(train_x, train_y)
+    #
+    # print("\nCalcul durata de timp...")
+    # start = time.process_time()
+    # score = neight.score(test_x, test_y)
+    # end = time.process_time()
+    # # print(f'{start}  {end}')
+    # print(f'{end - start} secunde')
+    #
+    # print(f'Scorul este: {score}')
 
-    print("\nCalcul durata de timp...")
-    start = time.process_time()
-    score = neight.score(test_x, test_y)
-    end = time.process_time()
-    # print(f'{start}  {end}')
-    print(f'{end - start} secunde')
+    generate_population(train_x)
+    current_index = 0
+    for i in range(0, no_individuals):
+        neight = KNeighborsClassifier(n_neighbors=k, weights=my_weights)
+        neight.fit(train_x, train_y)
 
-    print(f'Scorul este: {score}')
+        start = time.process_time()
+        score = neight.score(test_x, test_y)
+        eval[i] = score
+        end = time.process_time()
+        print(f'{end - start} secunde')
+
+    t = 0
+    while t != 1:
+        current_index = 0
+        t += 1
+        # de adaugat elitismul
+
+        # roata norocului
+        total_fitness = 0
+        for i in range(0, no_individuals):
+            total_fitness += eval[i]
+
+        individual_sel_prob = []
+        for i in range(0, no_individuals):
+            individual_sel_prob.append(eval[i] / total_fitness)
+        print("individual_sel_prob", individual_sel_prob)
+
+        accumulated_sel_prob = []
+        for i in range(0, no_individuals):
+            accumulated_sel_prob.append(0)
+        for i in range(0, no_individuals - 1):
+            accumulated_sel_prob[i + 1] = accumulated_sel_prob[i] + individual_sel_prob[i]
+        print("accumulated_sel_prob", accumulated_sel_prob)
+
+        no_selected_individuals = 0
+        for i in range(0, no_individuals):
+            uniform_random = random.uniform(0, 1)
+            for j in range(0, no_individuals - 1):
+                if accumulated_sel_prob[j] < uniform_random <= accumulated_sel_prob[j + 1]:
+                    new_population.append(population[j])
+                    no_selected_individuals += 1
+                    break
+
+        print("nr indivizi selectati cu roata norocului", no_selected_individuals)
+
+        # mutatie
+        # for i in range(0, no_individuals):
+        #     new_population[i] = mutation(new_population[i])
 
 
 if __name__ == '__main__':
