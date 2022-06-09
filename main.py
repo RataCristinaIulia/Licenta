dd04584ae6a5cd4d7893929f518d1334cbc48a4c
Rata Cristina Iulia <cristinaiulia.rata@gmail.com>
2022-06-09 18:05:08 +0300

    complete ga

--- main.py
+++ main.py
@@ -94,6 +94,7 @@ def generate_population(train_set):
         eval.append(1)
         procentages = generate_procentages(train_set)
         population.append(procentages)
+        new_population.append(procentages)
 
 
 def my_weights(weights):
@@ -134,25 +135,18 @@ def mutation(individ):
 
 def crossing(crom1, crom2):
     prob = random.uniform(0, 1)
+    child1 = crom1
+    child2 = crom2
     if prob < cross_chance:
         crossing_poz = random.randint(0, len(crom1))
         child1 = crom1[0:crossing_poz] + crom2[crossing_poz:len(crom2)]
         child2 = crom2[0:crossing_poz] + crom1[crossing_poz:len(crom1)]
+    return child1, child2
 
 
 def genetic_algorithm():
-    global train_x, train_y, test_x, test_y, k, current_index
-    # neight = KNeighborsClassifier(n_neighbors=k, weights=my_weights)
-    # neight.fit(train_x, train_y)
-    #
-    # print("\nCalcul durata de timp...")
-    # start = time.process_time()
-    # score = neight.score(test_x, test_y)
-    # end = time.process_time()
-    # # print(f'{start}  {end}')
-    # print(f'{end - start} secunde')
-    #
-    # print(f'Scorul este: {score}')
+    global train_x, train_y, test_x, test_y, k, current_index, population
+    best_score = 0
 
     generate_population(train_x)
     current_index = 0
@@ -163,16 +157,41 @@ def genetic_algorithm():
         start = time.process_time()
         score = neight.score(test_x, test_y)
         eval[i] = score
+        if score > best_score:
+            best_score = score
         end = time.process_time()
-        print(f'{end - start} secunde')
+        # print(f'{end - start} secunde')
 
     t = 0
-    while t != 1:
+    while t != 1000:
+        print("IN WHILE")
         current_index = 0
         t += 1
-        # de adaugat elitismul
 
-        # roata norocului
+        # ELITISM (primii doi indivizi din noua populatie vor fi cei mai buni doi indivizi din populatia anterioara)
+        print("IN ELITISM")
+        first_best_position = 0
+        second_best_position = 0
+
+        first_best_score = 0
+        second_best_score = 0
+        for i in range(0, no_individuals):
+            neight = KNeighborsClassifier(n_neighbors=k, weights=my_weights)
+            neight.fit(train_x, train_y)
+
+            score = neight.score(test_x, test_y)
+            if score > first_best_score:
+                second_best_score = first_best_score
+                second_best_position = first_best_position
+                first_best_position = i
+            elif score > second_best_score:
+                second_best_score = score
+                second_best_position = i
+        new_population[0] = population[first_best_position]
+        new_population[1] = population[second_best_position]
+
+        # ROATA NOROCULUI
+        print("IN ROATA")
         total_fitness = 0
         for i in range(0, no_individuals):
             total_fitness += eval[i]
@@ -180,29 +199,47 @@ def genetic_algorithm():
         individual_sel_prob = []
         for i in range(0, no_individuals):
             individual_sel_prob.append(eval[i] / total_fitness)
-        print("individual_sel_prob", individual_sel_prob)
+        # print("individual_sel_prob", individual_sel_prob)
 
         accumulated_sel_prob = []
         for i in range(0, no_individuals):
             accumulated_sel_prob.append(0)
         for i in range(0, no_individuals - 1):
             accumulated_sel_prob[i + 1] = accumulated_sel_prob[i] + individual_sel_prob[i]
-        print("accumulated_sel_prob", accumulated_sel_prob)
+        # print("accumulated_sel_prob", accumulated_sel_prob)
 
-        no_selected_individuals = 0
-        for i in range(0, no_individuals):
+        poz = 2
+        for i in range(2, no_individuals):
             uniform_random = random.uniform(0, 1)
             for j in range(0, no_individuals - 1):
                 if accumulated_sel_prob[j] < uniform_random <= accumulated_sel_prob[j + 1]:
-                    new_population.append(population[j])
-                    no_selected_individuals += 1
+                    new_population[poz] = population[j]
+                    poz += 1
                     break
 
-        print("nr indivizi selectati cu roata norocului", no_selected_individuals)
+        # MUTATIE
+        print("INAINTE DE MUTATIE")
+        for i in range(0, no_individuals):
+            new_mutant_ind = mutation(new_population[i])
+            new_population[i] = new_mutant_ind
 
-        # mutatie
-        # for i in range(0, no_individuals):
-        #     new_population[i] = mutation(new_population[i])
+        # CROSSING
+        print("INAINTE DE CROSSING")
+        for i in range(0, no_individuals - 1, 2):
+            new_population[i], new_population[i + 1] = crossing(new_population[i], new_population[i + 1])
+
+        population = new_population.copy()
+        current_index = 0
+
+        # EVALUARE
+        for i in range(0, no_individuals):
+            neight = KNeighborsClassifier(n_neighbors=k, weights=my_weights)
+            neight.fit(train_x, train_y)
+            score = neight.score(test_x, test_y)
+            if score > best_score:
+                best_score = score
+
+        print("scorul: ", best_score)
 
 
 if __name__ == '__main__':
@@ -213,5 +250,5 @@ if __name__ == '__main__':
     train_x, test_x, train_y, test_y = split_instances(instances, classes, 0.8)
 
     print(f'Lungime set de antrenare: {len(train_x)}\nLungime set de testare: {len(test_x)}')
-
+    print("Incepe alg genetic...")
     genetic_algorithm()
